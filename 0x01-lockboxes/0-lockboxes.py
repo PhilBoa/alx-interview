1728b8527d5cdbf9a12aaed830f6d3091859cdda
PhilBoa <philicbrefo98@gmail.com>
2023-12-05 21:09:55 +0000

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 
 """
-0-lockboxes Module
-
 Module for the canUnlockAll function to determine if all the boxes can be opened.
-
-Usage:
-    The canUnlockAll function takes a list of lists (boxes) as input,
-    where each inner list represents a box, and each box may contain keys
-    to other boxes. It returns True if all boxes can be opened, otherwise
-    False.
 """
 
 
