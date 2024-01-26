3946ea62aa4583b6c9696ae388e4c7ca0e16c6bd
PhilBoa <philicbrefo98@gmail.com>
2024-01-26 05:39:12 +0000

    0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-""" This module solves the Rotate 2D Matrix problem.
-The Rotate 2D Matrix problem involves placing 2D non-attacking on achessboard.
- """
- import sys
 
 def rotate_2d_matrix(matrix):
     n = len(matrix)
 
+    # Transpose the matrix
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
+    # Reverse each row
     for row in matrix:
         row.reverse()
 
+# Example usage:
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
@@ -22,4 +21,5 @@ matrix = [
 
 rotate_2d_matrix(matrix)
 print(matrix)
+
 
